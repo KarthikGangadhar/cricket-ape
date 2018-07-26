b92f2147c85dddf0e9ba5adce79d98f352e70059
Karthik G <karthikg@BRIMACLAP040.local>
2018-07-26 15:53:17 +0530

    refactored code

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -4,7 +4,6 @@ require 'news/news.rb'
 class NewsController < ApplicationController
   def show      
       cricApires = CricApi::Request.new('https://apecricket.herokuapp.com', 0)
-      binding.pry
       @news = News.new(cricApires.getNews)
       # @news = cricApires.jsonRead('./lib/cricket_api/json_data/news.json')
   end
